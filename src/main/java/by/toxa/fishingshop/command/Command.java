f4561bdcf76c8e021053e93127c4b63d2a62f6fd
= <=>
2021-08-21 17:41:40 +0300

    delete

--- src/main/java/by/toxa/fishingshop/command/Command.java
+++
@@ -1,8 +0,0 @@
-package by.toxa.fishingshop.command;
-
-import javax.servlet.http.HttpServletRequest;
-
-@FunctionalInterface
-public interface Command {
-    Router execute(HttpServletRequest request);
-}
