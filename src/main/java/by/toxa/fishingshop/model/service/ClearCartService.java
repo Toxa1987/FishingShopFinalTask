ada195d7efd48c5d783cd1ace09007af3e5883ee
Toxa1987 <wertyal.87@mail.ru>
2021-08-26 14:35:48 +0300

    delete

--- src/main/java/by/toxa/fishingshop/model/service/ClearCartService.java
+++
@@ -1,8 +0,0 @@
-package by.toxa.fishingshop.model.service;
-
-import by.toxa.fishingshop.model.entity.Cart;
-import by.toxa.fishingshop.exception.ServiceException;
-
-public interface ClearCartService {
-    void clearCart(Cart cart) throws ServiceException;
-}
